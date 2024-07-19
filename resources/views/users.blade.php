a16d398e3480b306b13f41f546f43bb543b14df5
FAIZAN HAFEEZ <mfaizan.hafeez@arhamsoft.org>
2024-07-19 17:21:05 +0500

    updated user page

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,28 +4,37 @@
 <div class="container-fluid px-4">
     <h1 class="mt-4">Users</h1>
     <div class="row">
-        <table class="table table-bordered data-table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="3">There are no users.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>Users
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered data-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="3">There are no users.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
